53ffcf387fab9812cf21a32621200f1a9b38c4dc
Kangkai Tang <kangkaitang0705@gmail.com>
2013-12-17 09:46:59 +0800

    1.2 Finished

--- SolutionForOne.java
+++ SolutionForOne.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 
 public class SolutionForOne {
 
-	
 	// 1.1
 	public boolean isUnique(String s) {
 		boolean[] byteUniqueArray = new boolean[128];
@@ -22,18 +21,30 @@ public class SolutionForOne {
 
 		return true;
 	}
-	
-	
+
 	// 1.2
 	public void reverse(char[] str) {
-		
+		int length = str.length - 1;
+		for (int i = 0; i < str.length / 2; i++) {
+			int left = i;
+			int right = length - i;
+
+			char temp = str[left];
+			str[left] = str[right];
+			str[right] = temp;
+		}
 	}
-	
-	
+
 	@Test
-	public void test() {
+	public void test1() {
 		System.out.println(isUnique("great"));
 	}
-	
-	
+
+	@Test
+	public void test2() {
+		char[] myChar = "hello".toCharArray();
+		System.out.println(myChar);
+		reverse(myChar);
+		System.out.println(myChar);
+	}
 }
