013855c68dcfc11db1a47a82b293d183fe2ef95e
Kangkai Tang <tangkangkai@gmail.com>
2014-01-27 20:07:53 -0800

    3.1 Fixed Divisions Finished

--- SolutionForThree.java
+++ SolutionForThree.java
@@ -4,7 +4,36 @@ import org.junit.Test;
 
 public class SolutionForThree {
 
-	//3.1
+	// 3.1
+	// Fixed Division
+	class threeStack {
+		int stackSize = 100;
+		int[] buffer = new int[stackSize * 3];
+		int[] pointer = new int[] { -1, -1, -1 };
+		
+		public void push(int stackNum, int value) throws Exception {
+			if(pointer[stackNum] +1 >= stackSize) {
+				throw new  Exception("OverFlow");
+			}
+			pointer[stackNum]++;
+			buffer[absTopOfStack(stackNum)] = value;
+		}
+		
+		public int pop(int stackNum) throws Exception{
+			if(pointer[stackNum] == -1){
+				throw new Exception("there is no element");
+			}
+			
+			int result = buffer[absTopOfStack(stackNum)];
+			buffer[absTopOfStack(stackNum)] = 0;
+			pointer[stackNum] --;
+			return result;
+		}
+		
+		int absTopOfStack(int stackNum) {
+			return stackSize * stackNum + pointer[stackNum];
+		}
+	}
 
 	@Test
 	public void test1() {
@@ -13,5 +42,63 @@ public class SolutionForThree {
 	@Test
 	public void generalTest() {
 	}
-}
 
+	class Node {
+		Object data;
+		Node next;
+
+		public Node(Object d) {
+			this.data = d;
+		}
+	}
+
+	class Stack {
+		Node top;
+
+		public Object pop() {
+			if (top != null) {
+				Object item = top.data;
+				top = top.next;
+				return item;
+			}
+			return null;
+		}
+
+		public void push(Object item) {
+			Node newNode = new Node(item);
+			newNode.next = top;
+			top = newNode;
+		}
+
+		public Object peek() {
+			return top.data;
+		}
+
+	}
+
+	class Queue {
+		Node first, last;
+
+		public void enqueue(Object item) {
+			Node newNode = new Node(item);
+			if (first == null) {
+				last = newNode;
+				first = last;
+			} else {
+				last.next = newNode;
+				last = last.next;
+			}
+
+		}
+
+		public Object dequeue() {
+			if (first != null) {
+				Object item = first.data;
+				first = first.next;
+				return item;
+			} else {
+				return null;
+			}
+		}
+	}
+}
